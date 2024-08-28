bd68161c65d582da85a95562131e225974e73b8b
Nikhil Maurya <164001640+NikhilxMaurya@users.noreply.github.com>
2024-08-28 19:30:04 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict" //treat all JS code as newer version
+// alert(3+2) //we are using node js, not browser
+let name = "Nikhil" //String
+let age = 20 //Number
+let isLoggedin = true //Boolean
+console.log(name, age, isLoggedin);
+console.log(typeof undefined);
+
+// datatypes:
+// Number
+// String
+// Boolean
+// Null
+// Undefined
+// Symbol => Uniqueness of component
+
+// Object
