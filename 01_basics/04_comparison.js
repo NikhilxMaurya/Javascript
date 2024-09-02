4a60ea9231a03b58c6764ab9ce41e99f2c38efee
Nikhil Maurya <164001640+NikhilxMaurya@users.noreply.github.com>
2024-09-02 17:58:19 +0000

    Comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,8 @@
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// Comparison and equality operators works differently as comparison operator converts null to number, treating it as 0.
+// === Used for strict check,checks equality as well as datatype.
+console.log("2" == 2);
+console.log("2" === 2);
+
