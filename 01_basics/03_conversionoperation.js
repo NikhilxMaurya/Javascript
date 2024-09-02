d62792cd4ccfb015144bccf98d0a2c01cb20b037
Nikhil Maurya <164001640+NikhilxMaurya@users.noreply.github.com>
2024-09-02 15:54:12 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,32 @@
+"use strict";
+let score = undefined
+console.log(typeof(score))
+let conversion = Number(score)
+console.log(typeof(conversion));
+console.log(score,conversion);
+
+// "33" => 33
+// "Nikhil"/"33abc" => NaN
+// "Null" => 0
+// "true" => 1
+
+// let isLoggedin = 1
+// let boolisLoggedin = Boolean(isLoggedin)
+// console.log(typeof(boolisLoggedin));
+// console.log(typeof(isLoggedin));
+
+
+
+// console.log(isLoggedin,boolisLoggedin);
+
+// 1 = true
+// 0 = false
+// "Nikhil" = true
+// "" = false
+// let num = 56
+// let constring = String(num)
+// console.log(typeof(num));
+// console.log(typeof(constring));
+// console.log(num,constring);
+
+
