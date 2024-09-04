a3379b6791328c34d034b1f9561941224fcb666b
Nikhil Maurya <164001640+NikhilxMaurya@users.noreply.github.com>
2024-09-04 18:23:12 +0000

    Datatypes Summary and Memory

--- 01_basics/Datatypes_summary.js
+++ 01_basics/Datatypes_summary.js
@@ -6,7 +6,7 @@ const temp = null //null
 let useremail; //undefined
 const id = Symbol('123')
 const anotherid = Symbol('123') //Symbol
-console.log(id === anotherid); 
+// console.log(id === anotherid); 
 const bigNumber = 123456473372263n //Bigint
 
 // Non - Primitive(Reference)
@@ -16,6 +16,33 @@ let myobj = {
     age : 20
 } //Objects(in key-value pairs inside curlybraces)
 const myfunction = function(){
-    console.log("Hello world");
+    // console.log("Hello world");
     
 } //function
+
+// ****************** Memory ************************ //
+
+// Stack (Primitive) and Heap (Non - Primitive)
+
+let myName = "Nikhil"
+
+let anotherName = myName
+anotherName = "Nik"
+// console.log(myName);
+// console.log(anotherName);
+
+let user01 = {
+    email : "abc@gmail.com",
+    Phoneno : 12345
+
+}
+console.log(user01);
+let user02 = user01
+user02.email = "xyz@gmail.com"
+console.log(user01.email);
+console.log(user02.email);
+
+
+
+
+
