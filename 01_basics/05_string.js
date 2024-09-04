a443fd8dfb8641c1c2df80622a0f8efb44883f62
Nikhil Maurya <164001640+NikhilxMaurya@users.noreply.github.com>
2024-09-04 19:22:33 +0000

    Strings and its Methods

---
+++ 01_basics/05_string.js
@@ -0,0 +1,23 @@
+const name = "Nikhil"
+const age = 20
+// console.log(name + age + " years");
+console.log(`hi my name is ${name} and my age is ${age}`);
+const Name = new String("Nikhil")
+console.log(Name.indexOf("k"));
+console.log(Name.length);
+console.log(Name.toUpperCase())
+console.log(Name.substring(0,3));
+console.log(Name.slice(-5,-2));
+const newName = "   Nikhil   "
+console.log(newName);
+console.log(newName.trim());
+const url = "https://Nikhil.com//nikhil20Maurya"
+console.log(url.replace("20"," - "))
+
+const game = new String("A-B-C-D-E")
+console.log(game.split("-"));
+
+
+
+        
+    
